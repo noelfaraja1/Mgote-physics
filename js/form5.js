431ffee6971f3f5e6da37aef8fab39a0163b928d
Noe_boe <112941400+noelfaraja1@users.noreply.github.com>
2023-01-04 03:20:51 -0800

    Initial commit

---
+++ js/form5.js
@@ -0,0 +1,8 @@
+alert("you ,welcome to my website");
+document.getElementsByClassName("dropdown-toggle").onClick = function(dropdown) {
+
+}
+
+document.addEventListener("dropdown-toggle") =function(dropdown) {
+
+}
